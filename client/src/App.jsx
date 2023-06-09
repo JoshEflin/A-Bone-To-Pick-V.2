4b4b8c9cf630af076fce81c7f54a26db81473009
ragingpenguin <joshuahuntereflin@gmail.com>
2023-06-09 13:41:39 -0400

    adding template to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,91 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './card.css'
 import defaultDog from './assets/images/default-dog.png'
+import Template from './components/Template'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-    <section>
-      <div data-id="64812450" class="card">
-          <div class="card-border">
-            <div class="card-header">
-              <span class="name">
-                Bridget
-              </span>
-              <span class="breed">
-                Shepherd
-              </span>
-            </div>
-            <div class="card-header2">
-              <span class="age">
-                Adult
-              </span>
-              <span class="gender">
-                Female
-              </span>
-            </div>
-            <img src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/64812450/1/?bust=1685641142" alt="photo of Doggo" />
-            <div class="dog-attributes">
-              <span class="size">
-                size: Large
-              </span>
-              <span class="house-trained">
-                <i class=" fa-solid fa-poop"></i>
-              </span>
-            </div>
-            <div class="dog-stats">
-              <div class="description">
-                <span class="item">
-                  Energy:
-                </span>
-                <span class="item">
-                  Trainability:
-                </span>
-                <span class="item">
-                  Playfulness:
-                </span>
-                <span class="item">                        
-                  Protectiveness:
-                </span>
-                <span class="item evil">
-                  Barking
-                </span>
-              </div>
-              <div class="power-level">
-                <span class="item2">
-                  <i class="fa-solid fa-bolt-lightning"></i>
-                  <i class="fa-solid fa-bolt-lightning"></i>
-                  <i class="fa-solid fa-bolt-lightning"></i>
-                </span>
-                <span class="item2">
-                  <i class=" fa-solid fa-scale-balanced"></i>
-                  <i class=" fa-solid fa-scale-balanced"></i>
-                </span>
-                <span class="item2">
-                  <i class=" fa-solid fa-face-grin-tears"></i>
-                  <i class=" fa-solid fa-face-grin-tears"></i>
-                  <i class=" fa-solid fa-face-grin-tears"></i>
-                </span>
-                <span class="item2">
-                  <i class="fa-solid fa-shield"></i>
-                  <i class="fa-solid fa-shield"></i>
-                  <i class="fa-solid fa-shield"></i>
-                  <i class="fa-solid fa-shield"></i>
-                  <i class="fa-solid fa-shield"></i>
-                 </span>
-                <span class="item2">
-                  <i class="fa-solid fa-volume-high"></i>
-                  <i class="fa-solid fa-volume-high"></i>
-                  <i class="fa-solid fa-volume-high"></i>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-    </section>
-    
+    <Template />
   )
 }
 
