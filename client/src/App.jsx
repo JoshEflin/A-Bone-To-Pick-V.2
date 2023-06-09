c4a0724b1223474122ddc8fe49fb4ea649e706bb
ragingpenguin <joshuahuntereflin@gmail.com>
2023-06-09 12:29:35 -0400

    added necessary photos, css and old files to commence refactoring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,81 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './card.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <div class="card ">
+            <div class="card-border foil">
+                <div class="card-header">
+                    <span class="name">Slim</span>
+                    <span class="breed">type: Greybutt</span>
+                </div>
+                <div class="card-header">
+                    <span class="age">Age :4</span>
+                </div>
+                <div>
+                    <img src="./assets/images/default-dog.png" />
+                    {/* <!-- <img src="https://api-ninjas.com/images/dogs/greyhound.jpg" alt="picture of a DOG"> --> */}
+                </div>
+                <div class="dog-attributes">
+                    <span class="size">55lbs</span>
+                    <span class="house-trained">3ft </span>
+                </div>
+
+                <div class="dog-stats">
+                    <div class="description">
+                        <span class="item">friendliness:</span>
+                        <span class="item">trainability:</span>
+                        <span class="item">pizazz:</span>
+
+                    </div>
+                    <div class="power-level">
+                        <span class="item2">a a a a a</span>
+                        <span class="item2">b b b</span>
+                        <span class="item2">c c c c c </span>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="card ">
+            <div class="card-border foil">
+                <div class="card-header">
+                    <span class="name">Slim</span>
+                    <span class="breed">type: Greybutt</span>
+                </div>
+                <div class="card-header">
+                    <span class="age">Age :4</span>
+                </div>
+                <div>
+                    <img src="./assets/images/default-dog.png" />
+                    {/* <!-- <img src="https://api-ninjas.com/images/dogs/greyhound.jpg" alt="picture of a DOG"> --> */}
+                </div>
+                <div class="dog-attributes">
+                    <span class="size">55lbs</span>
+                    <span class="house-trained">3ft </span>
+                </div>
+
+                <div class="dog-stats">
+                    <div class="description">
+                        <span class="item">friendliness:</span>
+                        <span class="item">trainability:</span>
+                        <span class="item">pizazz:</span>
+
+                    </div>
+                    <div class="power-level">
+                        <span class="item2">a a a a a</span>
+                        <span class="item2">b b b</span>
+                        <span class="item2">c c c c c </span>
+                    </div>
+                </div>
+            </div>
+          </div>
+    </section>
     </>
   )
 }
