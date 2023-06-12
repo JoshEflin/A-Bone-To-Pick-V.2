c180011662d5ff41b1eb9d84954d3111461207cb
LearnedDr <fredrickj.chang@gmail.com>
2023-06-11 23:40:19 -0400

    typedefs mutations possibly finished, resolvers continued, models/users added password hooks

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,8 @@
 // taken from brew buddies to get started, will change typedefs to fit bone2pick
+
+// input AddressInput and input ContactInput are needed in the mutation of dog info in general because 
+// (according to chatgpt: In summary, the Contact type represents the output representation of contact information, 
+// while the ContactInput input type represents the input representation for creating or updating contact information.)
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -20,11 +24,23 @@ const typeDefs = gql`
     address2: String
   }
 
+  input AddressInput {
+    address1: String
+    address2: String
+  }
+
+  input ContactInput {
+    email: String
+    phone: String
+    address: AddressInput
+  }
+
   type Contact {
     email: String
     phone: String
     address: Address
   }
+
   type Dog {
     _id: ID
     id: Int!
@@ -33,7 +49,7 @@ const typeDefs = gql`
     sex: String
     photo: String
     breed: String
-    size : String
+    size: String
     trained: Boolean
     contact: Contact
     description: String
@@ -61,7 +77,46 @@ const typeDefs = gql`
     dogById(dogID: String, breed: String): Dog
   }
 
-  
+  type Mutation {
+    addUser(
+      username: String!
+      email: String!
+      password: String!
+    ): Auth
+    login(
+      email: String!
+      password: String!
+    ): Auth
+    addDog(
+      _id: ID
+      id: Int!
+      name: String
+      age: String
+      sex: String
+      photo: String
+      breed: String
+      size: String
+      trained: Boolean
+      contact: ContactInput
+      description: String
+      status: String
+      energy: String
+      playfulness: Int
+      protectiveness: Int
+      trainability: Int
+      barking: Int
+      minHeightFemale: Int
+      maxHeightFemale: Int
+      minWeightFemale: Int
+      maxWeightFemale: Int
+      minHeightMale: Int
+      maxHeightMale: Int
+      minWeightMale: Int
+      maxWeightMale: Int
+    ): Auth
+  }
 `;
 
 module.exports = typeDefs;
+
+
