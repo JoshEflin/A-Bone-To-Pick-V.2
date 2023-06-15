7f00150861180445e88f521a09dc971c7c822db7
ragingpenguin <eflinjh@gmail.com>
2023-06-15 16:28:27 -0400

    honky tonky

--- client/src/components/cards/DogCard.jsx
+++ client/src/components/cards/DogCard.jsx
@@ -1,4 +1,4 @@
-import { GET_BY_ID, GET_BY_ZIP } from "../../utils/mutations";
+import { GET_BY_ID, GET_BY_ZIP,RESCUE_DOG_TO_DB } from "../../utils/mutations";
 import Template from "./Template.jsx"
 import { useState } from "react";
 import { useMutation,useQuery } from "@apollo/client";
@@ -6,12 +6,18 @@ import { useMutation,useQuery } from "@apollo/client";
 
  export default function DoggyDash (){
   const [zipString, setZipString] = useState("")
+  const [idString , setIdString]=useState("")
   const [breedString, setBreedString] = useState("")
   const [cardSelectedIndex, isCardSelectedIndex]= useState('-1');
   const [dogCardData, setDogCardData] = useState(null)
   const [DogsByZip, {error:errorZip, data:dataZip}] = useMutation(GET_BY_ZIP);
 
   // const [DogsById, {error:errorId, data:dataId}] = useMutation(GET_BY_ID);
+  const saveDog = (data)=> {
+    const [saveMyDogCard, {error:errorDogCard, data:dataDogCard}]= useMutation(RESCUE_DOG_TO_DB,{
+      variables: {...data}
+    })
+  }
   const handleZipSearch = (event)=>{
     const {name, value}= event.target;
     setZipString(value)
@@ -41,6 +47,7 @@ import { useMutation,useQuery } from "@apollo/client";
 
 
   return (
+    <>
    <form action="submit">
     <h2> SEARCH ME</h2>
     <label htmlFor="dog-search"></label>
@@ -49,5 +56,7 @@ import { useMutation,useQuery } from "@apollo/client";
     <input type="text" onChange={handleBreedSearch} value = {breedString} />
     <button type = "submit" onClick={handleSearchSubmit}></button>
    </form>
+   < Template />
+   </>
   )
 }
