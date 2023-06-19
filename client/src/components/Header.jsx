2572cf3338cb4a1612474e3e8160088d20e3867d
LearnedDr <fredrickj.chang@gmail.com>
2023-06-19 17:49:20 -0400

    added color codes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,13 +5,18 @@ export default function Header() {
     return (
         <header>
             <Row>
+                <Col>
+                <img src ="../assets/images/dogiconedit.png"alt="Logo"/>
+                </Col>
+                <Col>
                 <Space
     direction="vertical"
     size="middle"
     style={{paddingBottom: '50px'}}
-  >
+    >
             
             </Space>
+      </Col>
             <h1>Bone To Pick</h1>
             </Row>
         </header>
