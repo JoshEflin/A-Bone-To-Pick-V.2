9b8fd7d31557bc8463aba96aab6b32f1610ab6e0
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-06-22 19:10:35 -0400

    ternary finished for querying the logged in user and checking to see if they have the dog saved already. If saved, display the share me button, if not display the save button.

--- client/src/components/cards/DoggyDash.jsx
+++ client/src/components/cards/DoggyDash.jsx
@@ -2,22 +2,29 @@ import { GET_BY_ID, GET_BY_ZIP, RESCUE_DOG_TO_DB } from "../../utils/mutations";
 import { useState, useEffect } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { Input, Form, Button, Modal } from "antd";
+import { GET_ME } from "../../utils/queries";
 import DogCards from "./DogCard";
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 export default function DoggyDash() {
   const [zipString, setZipString] = useState("");
   const [idString, setIdString] = useState("");
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   const [form] = Form.useForm();
   const [breedString, setBreedString] = useState("");
   const [cardSelectedIndex, setCardSelectedIndex] = useState(-1);
   const [dogCardData, setDogCardData] = useState(null);
   const [loggedInUser, setLoggedInUser] = useState(null);
-  const [rescueDogtoDB, {error}] = useMutation(RESCUE_DOG_TO_DB)
+  const [rescueDogtoDB, { error }] = useMutation(RESCUE_DOG_TO_DB);
   const [DogsByZip, { error: errorZip, data: dataZip }] =
     useMutation(GET_BY_ZIP);
-   
+  const {
+    loading: loadingMe,
+    error: meError,
+    data: meData,
+    refetch: refetchMe,
+  } = useQuery(GET_ME);
+
   // const [DogsById, {error:errorId, data:dataId}] = useMutation(GET_BY_ID);
   const saveDog = (data) => {
     const [saveMyDogCard, { error: errorDogCard, data: dataDogCard }] =
@@ -50,10 +57,10 @@ export default function DoggyDash() {
       console.error(e);
     }
   };
-  
+
   const handleCardSelect = (index) => {
-    setCardSelectedIndex(index);   
-    console.log(index) 
+    setCardSelectedIndex(index);
+    console.log(index);
     setShowModal(true);
   };
   const handleModalClose = () => {
@@ -61,7 +68,7 @@ export default function DoggyDash() {
     setShowModal(false);
   };
   const handleRescueDogtoDB = async () => {
-    let myDog = dogCardData.dogByZip[cardSelectedIndex]
+    let myDog = dogCardData.dogByZip[cardSelectedIndex];
     const contactData = {
       email: myDog.email,
       phone: myDog.phone,
@@ -81,65 +88,79 @@ export default function DoggyDash() {
           contact: contactData,
         },
       });
-        if (!data) {
-            throw new Error('Unable to add dog.');
-        }  
-        console.log("You saved them")
+      if (!data) {
+        throw new Error("Unable to add dog.");
+      }
+      console.log("You saved them");
+      refetchMe();
     } catch (err) {
       console.error(err);
-     
     }
-};
-  
- if (cardSelectedIndex === -1) {
-  return (
-    <>
-      <Form layout="vertical" form={form}>
-        <Form.Item label="Enter Zip Code" name="zipSearch">
-          <Input placeholder=" ZipCode" onChange={handleZipSearch} />
-        </Form.Item>
-        <Form.Item label="Enter Breed " name="BreedSearch">
-          <Input placeholder="Breed" onChange={handleBreedSearch} />
-        </Form.Item>
-        {/* <h2> SEARCH ME</h2> */}
-        {/* <label htmlFor="dog-search"></label>
+  };
+
+  if (cardSelectedIndex === -1) {
+    return (
+      <>
+        <Form layout="vertical" form={form}>
+          <Form.Item label="Enter Zip Code" name="zipSearch">
+            <Input placeholder=" ZipCode" onChange={handleZipSearch} />
+          </Form.Item>
+          <Form.Item label="Enter Breed " name="BreedSearch">
+            <Input placeholder="Breed" onChange={handleBreedSearch} />
+          </Form.Item>
+          {/* <h2> SEARCH ME</h2> */}
+          {/* <label htmlFor="dog-search"></label>
     <input type="text" onChange={(handleZipSearch)} value = {zipString} /> */}
-        {/* <label htmlFor="data"></label> */}
-        <Form.Item name="submit">
-          <Button type="primary" onClick={handleSearchSubmit}>
-            Search{" "}
-          </Button>
-        </Form.Item>
-        {/* <input type="text" onChange={handleBreedSearch} value = {breedString} />
+          {/* <label htmlFor="data"></label> */}
+          <Form.Item name="submit">
+            <Button type="primary" onClick={handleSearchSubmit}>
+              Search{" "}
+            </Button>
+          </Form.Item>
+          {/* <input type="text" onChange={handleBreedSearch} value = {breedString} />
     <button type = "submit" onClick={handleSearchSubmit}></button> */}
-      </Form>
-      <section>
-      <DogCards props={dogCardData} fn={handleCardSelect} index={cardSelectedIndex} />
-      </section>
-    </>
-  );
- } else {
-  if (Auth.loggedIn()) {
-  return (
-    <>
-   <Modal open={showModal} onCancel={handleModalClose} footer={null}>
+        </Form>
         <section>
-          <DogCards props={dogCardData} fn={handleCardSelect} index={cardSelectedIndex} />
+          <DogCards
+            props={dogCardData}
+            fn={handleCardSelect}
+            index={cardSelectedIndex}
+          />
         </section>
-        <Button>Rescue</Button>
-        {/* {console.log(dogCardData.dogByZip[cardSelectedIndex].contact)} */}
-        <div>{dogCardData.dogByZip[cardSelectedIndex].contact.email}</div>
-        <div>{dogCardData.dogByZip[cardSelectedIndex].contact.phone}</div>
-        <Button onClick={handleRescueDogtoDB}>Save to My Pack</Button>
-        <Button>Share me!</Button>
-        <section>More Info Here</section>
-      </Modal>
       </>
-  )
- } else {
-  return (
-    <h2>Please Log In!</h2>
-  )
- }}
-  
+    );
+  } else {
+    if (Auth.loggedIn()) {
+      return (
+        <>
+          <Modal open={showModal} onCancel={handleModalClose} footer={null}>
+            <section>
+              <DogCards
+                props={dogCardData}
+                fn={handleCardSelect}
+                index={cardSelectedIndex}
+              />
+            </section>
+            <Button>Rescue</Button>
+            {/* {console.log(dogCardData.dogByZip[cardSelectedIndex].contact)} */}
+            <div>{dogCardData.dogByZip[cardSelectedIndex].contact.email}</div>
+            <div>{dogCardData.dogByZip[cardSelectedIndex].contact.phone}</div>
+            {meData &&
+            meData.me.dogCards.some(
+              (dog) => dog.id === dogCardData.dogByZip[cardSelectedIndex].id
+            ) ? (
+              <Button>Share me!</Button>
+            ) : (
+              <Button onClick={handleRescueDogtoDB}>Save to My Pack</Button>
+            )}
+
+            {console.log(meData)}
+            <section>More Info Here</section>
+          </Modal>
+        </>
+      );
+    } else {
+      return <h2>Please Log In!</h2>;
+    }
+  }
 }
