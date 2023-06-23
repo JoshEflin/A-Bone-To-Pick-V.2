2e15489990214dc390e74b59a8ce8a7eaeea24ed
ragingpenguin <eflinjh@gmail.com>
2023-06-22 22:35:21 -0400

    fixed mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -18,37 +18,6 @@ export const LOGIN = gql`
 mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
-        username
-        email
-        password
-        dogCards {
-          id
-          name
-          age
-          sex
-          photo
-          breed
-          size
-          trained
-          description
-          status
-          energy
-          playfulness
-          protectiveness
-          trainability
-          barking
-          minHeightFemale
-          maxHeightFemale
-          minWeightFemale
-          maxWeightFemale
-          minHeightMale
-          maxHeightMale
-          minWeightMale
-          maxWeightMale
-        }
-      }
     }
   }`
 
