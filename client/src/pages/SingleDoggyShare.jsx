0d84dc1732087b3491ab3d652b343ffb8ad382f9
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-06-26 23:54:54 -0400

    dog and user cards showing on single dog page

--- client/src/pages/SingleDoggyShare.jsx
+++ client/src/pages/SingleDoggyShare.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { GET_DOG_BY_ID_DB } from "../utils/queries";
 import DogCards from "../components/cards/DogCard";
+import ProfileCard from "../components/cards/ProfileCard";
 
 const SingleDog = () => {
   const { id } = useParams();
@@ -13,19 +14,36 @@ const SingleDog = () => {
   } = useQuery(GET_DOG_BY_ID_DB, {
     variables: { id },
   });
-  console.log(dataDog?.dogDbById?.users)
+  console.log(dataDog?.dogDbById?.users[0]);
 
   const handleCardSelect = () => {
     console.log("hello world");
   };
 
+  if (loadingDog) {
+    // Optional: You can display a loading state here
+    return <div>Loading...</div>;
+  }
+
+  if (errorDog) {
+    // Optional: You can display an error message here
+    return <div>Error: {errorDog.message}</div>;
+  }
+
   return (
-    <DogCards
-      dogCardDataArray={dataDog?.dogDbById}
-      fn={handleCardSelect}
-      index={0}
-    />
-    
+    <>
+      <DogCards
+        dogCardDataArray={dataDog?.dogDbById}
+        fn={handleCardSelect}
+        index={0}
+      />
+
+      {dataDog?.dogDbById?.users &&
+        dataDog?.dogDbById?.users.length > 0 &&
+        dataDog?.dogDbById?.users.map((user) => (
+          <ProfileCard key={user.id} props={user} />
+        ))}
+    </>
   );
 };
 
