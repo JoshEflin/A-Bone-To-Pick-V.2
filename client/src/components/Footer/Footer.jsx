a724bafec4bda44945328d35b243c1b3c2e82025
ragingpenguin <eflinjh@gmail.com>
2023-06-28 16:27:48 -0400

    adding footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,6 +7,21 @@ export default function Foot() {
     <Footer>
       <h3>Bone To Pick is brought to you by:</h3>
       <p>Awesome Scorpions</p>
+      <div class="flex">
+        <div >
+          <button id="margGit" >
+            <img
+              class="dogicon" src="./assets/images/dogiconedit.png" />Margaret's Github</button>
+        </div>
+        <div class="column is-3">
+          <button id="fredGit" ><img
+              class="dogicon" src="./assets/images/dogiconedit.png" />Fred's Github</button>
+        </div>
+        <div class="column is-3">
+          <button id="joshGit" ><img
+              class="dogicon" src="./assets/images/dogiconedit.png" />Josh's Github</button>
+        </div>
+      </div>
     </Footer>
     </>
   );
